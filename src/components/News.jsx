7a44e1bb7f21dcb6e31e705c877a6bae2ee156db
mohsin19295 <mohsin19295@gmail.com>
2024-01-01 07:13:03 +0530

    infinite scroll added

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,63 +6,76 @@ import Snipper from "../../src/spinner.gif"
 function News(props) {
   const [post, setPost] = useState([])
   const [page, setPage] = useState(1)
-  // const [limit, setLimit] = useState(8)
-  const limit = 10;
-  const [total, setTotal] = useState(null)
-  const [loading, setLoading]= useState(false)
+  const [total, setTotal] = useState(0)
+  const [loading, setLoading] = useState(false)
+  
+  const fetchData = async () => {
+    const apiKey = "3e19ce7b891447459477f5ef54207823";
+    try {
+      props.setProgress(0)
+      setLoading(true);
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${apiKey}&page=${page}`);
+      console.log(response);
+      setPost(prePost => [...prePost, ...response?.data?.articles]);
+      setPage(pre => pre + 1);
+      setTotal(response?.data?.totalResults);
+      props.setProgress(100);
+    } catch (error) {
+      console.log('An error occurs while fetching', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      console.log(post.length < total);
+      if (!loading) {
+        fetchData();
+      }
+    }
+  };
 
   useEffect(() => {
-    const apiKey = "3e19ce7b891447459477f5ef54207823";
-    props.setProgress(0)
-    setLoading(true)
-    axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${apiKey}&page=${page}`)
-    .then(res=>{
-      console.log(res?.data?.articles);
-      setPost(res?.data?.articles)
-      setTotal(Math.ceil(res.data.totalResults/limit))
-      props.setProgress(100)
-    })
-    .catch(err=>{
-      console.log(err)
-    })
-    .finally(()=>setLoading(false))
+    window.addEventListener('scroll', handleScroll);
+    fetchData();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [page])
 
+  const formatDate = (dateString) => {
+    const options = {
+      weekday: 'short',
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZone: 'UTC',
+    };
+    return new Date(dateString).toLocaleString('en-US', options);
+  };
 
-  const handlePrevious =()=>{
-    console.log("previous")
-    setPage(page-1)
-  }
-
-  const handleNext =()=>{
-    console.log("next")
-    setPage(page+1)
-  }
-
-   return (
-    <>
-     <div id="container">
-     {loading && <img id="snipper" src={Snipper} alt="" /> }
-    
-     <div id="grid">
-    {post?.map(e=>(
-      <div key={e.id == null ? e.id=Math.random(1,100): e.id}>
-        <img src={e.urlToImage == null ? e.urlToImage = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" : e.urlToImage} alt="urlToImage" />
-          <p id="title">{e.title}</p>
-          <p id="author">{e.source.name == null ? e.source.name = "Unknown" : e.source.name}</p> 
-          <p id="date">{new Date(e.publishedAt).toGMTString()}</p>
-          <a id="readMore" href={e.url} target="_blank" rel="noreferrer" >Read more</a>
+  return (
+      <div id="container">
+        <div id="grid">
+          {post?.map(e => (
+            <div key={e.id == null ? e.id = Math.random(1, 100) : e.id}>
+              <img src={e.urlToImage == null ? e.urlToImage = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" : e.urlToImage} alt="urlToImage" />
+              <p id="title">{e.title}</p>
+              <p id="author">{e.source.name == null ? e.source.name = "Unknown" : e.source.name}</p>
+              <p id="date">{formatDate(e.publishedAt)}</p>
+              <a id="readMore" href={e.url} target="_blank" rel="noreferrer" >Read more</a>
+            </div>
+          ))}
       </div>
-    ))}
-    </div>
-
-      <div id="btnDiv">
-        <button className="preNextBtn" disabled={page <= 1} onClick={handlePrevious}>&larr; Previous</button>
-        <button className="preNextBtn" disabled={page >= total}  onClick={handleNext}>Next &rarr;</button>
+      {loading && <img id="snipper" src={Snipper} alt="spinner" />}
       </div>
-      
-       </div>
-    </>
   );
 }
 export default News
