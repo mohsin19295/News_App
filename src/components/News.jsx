aed2cefa8a8eba7560c4aa3cc5bcfddb7c0da89a
mohsin19295 <mohsin19295@gmail.com>
2024-01-05 10:42:18 +0530

    back to next-pre

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,46 +7,39 @@ function News(props) {
   const [post, setPost] = useState([])
   const [page, setPage] = useState(1)
   const [total, setTotal] = useState(0)
-  const [loading, setLoading] = useState(false)
-  
-  const fetchData = async () => {
+  const [loading, setLoading] = useState(false);
+  let limit = 10;
+
+  useEffect(() => {
     const apiKey = "3e19ce7b891447459477f5ef54207823";
     try {
       props.setProgress(0)
-      setLoading(true);
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${apiKey}&page=${page}`);
-      console.log(response);
-      setPost(prePost => [...prePost, ...response?.data?.articles]);
-      setPage(pre => pre + 1);
-      setTotal(response?.data?.totalResults);
-      props.setProgress(100);
+      setLoading(true)
+      axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${apiKey}&page=${page}`)
+        .then(res => {
+          console.log('data', res?.data?.articles)
+          setPost(res?.data?.articles)
+          setTotal(Math.ceil(res.data?.totalResults / limit))
+          props.setProgress(100)
+        })
     } catch (error) {
       console.log('An error occurs while fetching', error);
+
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
+  }, [page])
+
+
+  const handlePrevious = () => {
+    console.log("previous")
+    setPage(page - 1)
   }
 
-  const handleScroll = () => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight
-    ) {
-      console.log(post.length < total);
-      if (!loading) {
-        fetchData();
-      }
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    fetchData();
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [page])
+  const handleNext = () => {
+    console.log("next")
+    setPage(page + 1)
+  }
 
   const formatDate = (dateString) => {
     const options = {
@@ -62,20 +55,24 @@ function News(props) {
   };
 
   return (
-      <div id="container">
-        <div id="grid">
-          {post?.map(e => (
-            <div key={e.id == null ? e.id = Math.random(1, 100) : e.id}>
-              <img src={e.urlToImage == null ? e.urlToImage = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" : e.urlToImage} alt="urlToImage" />
-              <p id="title">{e.title}</p>
-              <p id="author">{e.source.name == null ? e.source.name = "Unknown" : e.source.name}</p>
-              <p id="date">{formatDate(e.publishedAt)}</p>
-              <a id="readMore" href={e.url} target="_blank" rel="noreferrer" >Read more</a>
-            </div>
-          ))}
-      </div>
+    <div id="container">
       {loading && <img id="snipper" src={Snipper} alt="spinner" />}
+      <div id="grid">
+        {post?.map(e => (
+          <div key={e.id == null ? e.id = Math.random(1, 100) : e.id}>
+            <img src={e.urlToImage == null ? e.urlToImage = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" : e.urlToImage} alt="urlToImage" />
+            <p id="title">{e.title}</p>
+            <p id="author">{e.source.name == null ? e.source.name = "Unknown" : e.source.name}</p>
+            <p id="date">{formatDate(e.publishedAt)}</p>
+            <a id="readMore" href={e.url} target="_blank" rel="noreferrer" >Read more</a>
+          </div>
+        ))}
       </div>
+      <div id="btnDiv">
+        <button className="preNextBtn" disabled={page <= 1} onClick={handlePrevious}>&larr; Previous</button>
+        <button className="preNextBtn" disabled={page >= total} onClick={handleNext}>Next &rarr;</button>
+      </div>
+    </div>
   );
 }
 export default News
